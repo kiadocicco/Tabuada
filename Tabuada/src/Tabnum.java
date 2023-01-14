f3060d217f3f0044bafe7962b55feec1dfd71180
kiadocicco <kiadocicco@yahoo.com.br>
2023-01-13 22:02:39 -0300

    vs1

---
+++ Tabuada/src/Tabnum.java
@@ -0,0 +1,15 @@
+
+public class Tabnum {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<11;i++) {
+			System.out.println("Tabuada do "+i);
+			for (int j=1;j<11;j++) {
+				int result=i*j;
+				System.out.println(i+"x"+j+"="+result);
+			}
+		}
+	}
+
+}
